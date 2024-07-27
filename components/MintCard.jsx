02242e79e4fbb9a0a8d5767c2d32d3cf6662390f
0xLuki <1255552127@qq.com>
2024-07-28 02:11:47 +0800

    fix quantity input w

--- components/MintCard.jsx
+++ components/MintCard.jsx
@@ -101,7 +101,7 @@ const GeneTable = ({ data, onMint }) => {
                   max="100"
                   value={quantities[index]}
                   onChange={(e) => handleQuantityChange(index, e.target.value)}
-                  className="w-14"
+                  className="w-14 sm:w-20"
                 />
               </TableCell>
               <TableCell>
